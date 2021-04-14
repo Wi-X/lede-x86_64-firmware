7e63cc864ccfc58a8e989e628a54237547b13a5f
Will <82292232+Wi-X@users.noreply.github.com>
2021-04-14 18:51:36 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #git clone https://github.com/Wi-X/openwrt-r8125 package/openwrt-r8125
+
+
+git clone https://github.com/jerrykuku/lua-maxminddb.git #git lua-maxminddb 依赖
+
+git clone https://github.com/jerrykuku/luci-app-vssr.git
